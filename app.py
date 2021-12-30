9d8947acb7ee30024289da70769595d60d77022a
ramreddy18 <96581900+ramreddy18@users.noreply.github.com>
2021-12-30 21:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Iris Classifier- API")
 
-sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)
+sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)    
 sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
 pl = st.slider('Petal Length', 1.0, 6.9, 0.5)
 pw = st.slider('Petal Width', 0.1,2.5, 0.5)
